8c3daac9cbc44822e9c01590553900d43e05bb9d
Ericson Joseph <ericsonjoseph@gmail.com>
2018-08-02 20:27:08 -0500

    Add initial files

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ * File:   main.c
+ * Author: Ericson Joseph
+ * 
+ * Created on August  2, 2018,  8:27 PM
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    
+}
