bb83f4cf294c902f76224f7e16ea97a01b8ae2f5
Ericson Joseph <ericsonjoseph@gmail.com>
2018-12-06 11:21:48 -0500

    Fix sin_port display

--- main.c
+++ main.c
@@ -65,12 +65,12 @@ int main(int argc, char *argv[]) {
             char *dateTime = getFormatTime();
             if (!NE_exist(cname)) {
                 printf("%s\t%s\t%s:%d\tNEW.\n", dateTime, cname, ip,
-                       cliaddr.sin_port);
+                       ntohs(cliaddr.sin_port));
                 fflush(stdout);
                 NE_put(cname, cliaddr);
             } else {
                 printf("%s\t%s\t%s:%d\tKEEP-ALIVE.\n", dateTime, cname, ip,
-                       cliaddr.sin_port);
+                       ntohs(cliaddr.sin_port));
                 fflush(stdout);
                 NE_touch(cname, cliaddr);
             }
